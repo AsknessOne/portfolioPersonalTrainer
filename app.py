6b07e0f160f7dfea21093a33e6035f88b1a7b615
Cinthia Ferreira <fcinthia11@yahoo.com>
2026-01-11 22:48:45 -0300

    primeiro deploy flask

--- app.py
+++ app.py
@@ -1,30 +1,69 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+from flask import Flask, render_template, request, redirect, session
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'e153c8a11321db062e9e6f4706574dcd982af35a78e8a24d05c83d22eef4c9b2'
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+
+WHATSAPP_NUMBER = os.getenv("WHATSAPP_NUMBER")
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    show_modal = session.pop('show_modal', False)
+    return render_template('index.html', show_modal=show_modal)
 
 @app.route('/avaliacao', methods=['GET', 'POST'])
-def assessment():
+def avaliacao():
     if request.method == 'POST':
-        # Coleta de dados do formulário
-        dados_cliente = {
-            "nome": request.form.get('nome'),
-            "objetivo": request.form.get('objetivo'),
-            "peso": request.form.get('peso'),
-            # Aqui você poderia salvar em um banco de dados ou enviar por e-mail
-        }
-        
-        # Simulação de processamento de dados
-        print(f"Nova Consultoria Recebida: {dados_cliente}")
-        
-        flash('Obrigado, Renato entrará em contato em breve!', 'success')
-        return redirect(url_for('index'))
-    
+        # Dados do formulário
+        nome = request.form.get('nome')
+        idade = request.form.get('idade')
+        altura = request.form.get('altura')
+        peso = request.form.get('peso')
+        objetivo = request.form.get('objetivo')
+        atividade = request.form.get('atividade')
+        restricoes = request.form.get('restricoes')
+
+        # Mensagem formatada para o WhatsApp
+        mensagem = f"""
+Olá, Renato! 
+
+Acabei de preencher a pré-avaliação no seu site.
+Seguem meus dados:
+
+Nome: {nome}
+Objetivo: {objetivo}
+Nível de atividade: {atividade}
+
+Altura: {altura} cm
+Peso: {peso} kg
+Idade: {idade}
+
+Restrições:
+{restricoes if restricoes else "Nenhuma informada"}
+
+Gostaria de conversar sobre a consultoria personalizada.
+"""
+
+        texto = quote_plus(mensagem)
+
+        whatsapp_url = (
+            f"https://wa.me/{WHATSAPP_NUMBER}"f"?text={texto}"
+        )
+
+        return redirect(whatsapp_url)
+
+    # GET → abre o formulário
     return render_template('assessment.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+    
